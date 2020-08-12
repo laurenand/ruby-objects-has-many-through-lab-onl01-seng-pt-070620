9d95f20d9e2b6e6b42ddb025c07c439f7d7712e7
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-12 22:26:00 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,23 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_song(name, genre)
+    Song.new(name, genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+end
